132790733d62e15cac906561b2d473c4e2f5f1cb
sarika srivastava <sarikasrivastava2005@gmail.com>
2025-07-26 18:13:42 -0400

    Implemented TableHeader Component

---
+++ src/components/Table/TableHeader.tsx
@@ -0,0 +1,18 @@
+import type { AppColumnDef } from "../../types/app-column-def";
+import { TableCell } from "./TableCell";
+
+export interface TableHeaderProps<TData> {
+  columns: AppColumnDef<TData>[];
+}
+
+export function TableHeader<TData>({ columns }: TableHeaderProps<TData>) {
+  return (
+    <div className="table-row table-header">
+      {columns.map((col) => (
+        <TableCell key={col.id} header={col.header}>
+          {col.header}
+        </TableCell>
+      ))}
+    </div>
+  );
+}
