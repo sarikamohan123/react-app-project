22202307f6f54fd8b793c64cd78f4fc9259b9058
sarika srivastava <sarikasrivastava2005@gmail.com>
2025-08-07 19:16:14 -0400

    sync table state to read from and update URL using useSearchParams in useEntityListOption

--- src/hooks/useEntityListOptions.ts
+++ src/hooks/useEntityListOptions.ts
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function useEntityListOptions() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const entity = searchParams.get("entity") ?? "pokemon";
+  const limit = parseInt(searchParams.get("limit") ?? "10");
+  const offset = parseInt(searchParams.get("offset") ?? "0");
+
+  const setEntity = (value: string) => {
+    searchParams.set("entity", value);
+    searchParams.set("offset", "0");
+    setSearchParams(searchParams);
+  };
+
+  const setLimit = (value: number) => {
+    searchParams.set("limit", value.toString());
+    searchParams.set("offset", "0");
+    setSearchParams(searchParams);
+  };
+
+  const setOffset = (value: number) => {
+    searchParams.set("Offset", value.toString());
+    setSearchParams(searchParams);
+  };
+
   //getters
-  const [entity, setEntity] = useState("pokemon");
-  const [limit, setLimit] = useState(10);
-  const [offset, setOffset] = useState(0);
+  // const [entity, setEntity] = useState("pokemon");
+  // const [limit, setLimit] = useState(10);
+  // const [offset, setOffset] = useState(0);
   //setters
   const state = {
     entity,
