3e252e06de1662f96d09787f6f6edbf24223c283
sarika srivastava <sarikasrivastava2005@gmail.com>
2025-08-11 19:13:10 -0400

    used placedholderData fron TanStack query to resolve query lagging issue

--- src/hooks/useEntityList.ts
+++ src/hooks/useEntityList.ts
@@ -1,26 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 
 interface EntityListParams {
-    entity: string;
-    limit?: number;
-    offset?: number;
+  entity: string;
+  limit?: number;
+  offset?: number;
 }
 
-function useEntityList(params:EntityListParams) {
-    return useQuery({
-        enabled: !! params.entity,
-        queryKey: ["entityList", params.entity, params.limit,params.offset],
-        queryFn: async () => {
-            const url = `https://pokeapi.co/api/v2/${params.entity}?limit=${params.limit ?? 10}&offset=${params.offset ?? 0}`
-            const response =await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Error fetching entity list: ${response.statusText}`);
-            }
-            return response.json();
-        },
-        staleTime: 300000, // 5 minutes
-
-    })
+function useEntityList(params: EntityListParams) {
+  return useQuery({
+    enabled: !!params.entity,
+    queryKey: ["entityList", params.entity, params.limit, params.offset],
+    queryFn: async () => {
+      const url = `https://pokeapi.co/api/v2/${params.entity}?limit=${
+        params.limit ?? 10
+      }&offset=${params.offset ?? 0}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error fetching entity list: ${response.statusText}`);
+      }
+      return response.json();
+    },
+    staleTime: 300000, // 5 minutes
+    placeholderData: keepPreviousData,
+  });
 }
 export default useEntityList;
-
