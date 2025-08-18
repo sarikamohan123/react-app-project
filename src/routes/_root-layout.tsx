a02eb6dd75821d7b6e6a3230ab99ec7326a86a12
sarika srivastava <sarikasrivastava2005@gmail.com>
2025-08-17 22:41:41 -0400

    removed burger menu in the header

--- src/routes/_root-layout.tsx
+++ src/routes/_root-layout.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "@tanstack/react-router";
-import { AppShell, Burger, Group, Title } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { AppShell, Group, Title } from "@mantine/core";
 
 export function RootLayout() {
-  const [opened, { toggle }] = useDisclosure(false);
   return (
     <AppShell header={{ height: 60 }} padding="md">
       <AppShell.Header>
         <Group h="100%" px="md" justify="space-between">
-          <Burger opened={opened} onClick={toggle} size="sm" />
           <Title order={4}>PokeApi Table</Title>
         </Group>
       </AppShell.Header>
