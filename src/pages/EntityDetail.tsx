067caa925684fa21db5ff419139f0f59c8954d64
sarika srivastava <sarikasrivastava2005@gmail.com>
2025-08-18 23:01:48 -0400

    added pages EntityDetail and NotFound

---
+++ src/pages/EntityDetail.tsx
@@ -0,0 +1,51 @@
+import { useParams } from "@tanstack/react-router";
+import type { DetailsParams } from "../routes/detail-params";
+import { useQuery } from "@tanstack/react-query";
+import { Container, Title, Loader, Center, Alert, Code } from "@mantine/core";
+
+//  const Details_Route_Id = "/$entity/$id" as const;
+
+export default function EntityDetail() {
+  const { entity, id } = useParams({
+    from: "/$entity/$id" as const,
+  }) as DetailsParams;
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["entityDetail", entity, id],
+    queryFn: async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/${entity}/${id}`);
+      if (!response.ok)
+        throw new Error(
+          `Fetched failed : ${response.status} ${response.statusText}`
+        );
+      return response.json();
+    },
+    staleTime: 60_000,
+  });
+  if (isLoading) {
+    return (
+      <Container size="md" p="md">
+        <Center mih={200}>
+          <Loader />
+        </Center>
+      </Container>
+    );
+  }
+  if (isError) {
+    return (
+      <Container size="md" p="md">
+        <Alert color="red" title="Failed to Load">
+          {(error as Error).message}
+        </Alert>
+      </Container>
+    );
+  }
+  return (
+    <Container size="md" p="md">
+      <Title order={2} mb="sm">
+        {entity} #{id}
+      </Title>
+      <Code block>{JSON.stringify(data, null, 2)}</Code>
+    </Container>
+  );
+}
